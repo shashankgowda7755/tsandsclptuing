9c391ffd8e41d51392f165132752c095f818cbf8
shashankgowda7755 <shashankgowda7755@gmail.com>
2026-02-06 20:49:07 +0530

    Remove unused homepage sections: Initiatives, WhatWeDo, GetInvolved, FeaturedStory, Events, Gallery

--- components/HomeSections.tsx
+++ components/HomeSections.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import { MetricsSection } from './MetricsSection';
-import { GallerySection } from './GallerySection';
-import { InitiativesSection } from './InitiativesSection';
-import { WhatWeDoSection } from './WhatWeDoSection';
-import { GetInvolvedSection } from './GetInvolvedSection';
-import { FeaturedStorySection } from './FeaturedStorySection';
-import { EventSection } from './EventSection';
 import { Hero } from './Hero';
 
 interface HomeSectionsProps {
@@ -14,27 +7,11 @@ interface HomeSectionsProps {
   onInitiativeClick?: (id: string) => void;
 }
 
-export const HomeSections: React.FC<HomeSectionsProps> = ({ onJoin, onStart, onInitiativeClick }) => {
+export const HomeSections: React.FC<HomeSectionsProps> = ({ onStart }) => {
   return (
     <div className="flex flex-col gap-0">
       {/* New Hero Section */}
       <Hero onStart={onStart || (() => {})} />
-      
-      <InitiativesSection onJoin={onJoin} onInitiativeClick={onInitiativeClick} />
-      
-      <WhatWeDoSection />
-      
-      <GetInvolvedSection onJoin={onJoin} />
-      
-      <FeaturedStorySection />
-
-      <EventSection onJoin={onJoin} />
-
-      <GallerySection />
-
-
-
-      <GallerySection />
     </div>
   );
 };
